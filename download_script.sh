57008380da01a9afb33ab30b8770eda217b0d1ac
genetics <luk.kuprys@gmail.com>
2026-04-12 22:16:49 +0300

    initial files and download script

---
+++ download_script.sh
@@ -0,0 +1,11 @@
+# wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/049/SRR11647649/SRR11647649_1.fastq.gz
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/049/SRR11647649/SRR11647649_2.fastq.gz
+
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/051/SRR11647651/SRR11647651_1.fastq.gz
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/051/SRR11647651/SRR11647651_2.fastq.gz
+
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/059/SRR11647659/SRR11647659_1.fastq.gz
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/059/SRR11647659/SRR11647659_2.fastq.gz
+
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/061/SRR11647661/SRR11647661_1.fastq.gz
+wget -P ~/raw_data ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/061/SRR11647661/SRR11647661_2.fastq.gz
